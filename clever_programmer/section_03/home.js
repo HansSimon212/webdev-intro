7f5e0c4f6beead91b94ea26aa4e678e82693081c
Hans Simon <simon.h@husky.neu.edu>
2020-06-17 19:55:15 -0400

    Adds to section_03
    Adds index.html, home.js to section_03

---
+++ clever_programmer/section_03/home.js
@@ -0,0 +1,17 @@
+let num1 = 5;       //integer
+let num2 = 4.0;     //floating point
+
+//console.log(5.7);
+//console.log(5 / 2); // NOTE: not integer division
+
+num1 = num1 + num2;
+
+// Functions in JavaScript
+function warning() {
+    alert("Clean up your room, young man!");
+}
+
+// calls newly created function
+warning();
+
+// examples of some datatypes
