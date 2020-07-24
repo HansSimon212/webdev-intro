465da8f881550e56217c6b0e21309788c308342d
Hans <simon.h@husky.neu.edu>
2020-07-24 13:12:08 -0400

    Removes duplicate top-level javascript directory.

--- javascript/caleb_curry/blue/main.js
+++
@@ -1,6 +0,0 @@
-// If the user confirms her purchase, reflect that their order
-// has been placed successfully.  Removes confirm button.
-document.getElementById('confirm_button').onclick = function () {
-    document.getElementById('confirm_prompt').innerHTML = 'Order Placed Successfully!'
-    document.getElementById("confirm_button").style.display = "none";
-}
